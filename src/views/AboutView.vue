8236c7c5d299c19941f2bbf02a2d3ce39b40b5f2
MirrorML <2867405686@qq.com>
2022-09-19 11:04:46 +0800

    1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,10 +2,13 @@
   <div id="building">
     <div class="about">
       <h1>SE331-2022-Project-1</h1>
-      <h4>Github Link: https://github.com/ShengzheH/SE331-2022-Project-1</h4>
+      <h4>
+        Github Link:
+        https://github.com/SE331-Componet-Based-Software-Develop/SE331-2022-Project-1
+      </h4>
       <h4>Group member:</h4>
-      <h4>649529448 Shengzhe Huang | Branch name: ShengzheH</h4>
-      <h4>649529444 Haolong Huai | Branch name: HaolongH</h4>
+      <h4>649529440 Mingming Jia</h4>
+      <h4>649529442 Zhenxv Wang</h4>
     </div>
   </div>
 </template>
